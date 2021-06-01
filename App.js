db0cd6472fa0d10b4ee5635ef2e279d9dfab5617
apriadi <dev.apriadi@gmail.com>
2021-06-01 21:11:15 +0700

    menyelipkan provider

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppStack from './src/stack/AppStack'
+import {GlobalProvider} from './src/provider/GlobalProvider'
 
 
 const App = () => { 
   return(
-    <NavigationContainer>
-      <AppStack/>
-    </NavigationContainer>
+    <GlobalProvider>
+      <NavigationContainer>
+        <AppStack/>
+      </NavigationContainer>
+    </GlobalProvider>
   )
 };
 
